e9eeea267428d4d3969d0c6eed33ade2d8e31ec3
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-06-21 20:43:55 +0100

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+
+            maxFreq = Math.max(maxFreq, freqMap.get(c));
+
+            int windowSize = right - left + 1;
+
+            // invalid window
+            if (windowSize - maxFreq > k) {
+                char leftChar = s.charAt(left);
+                freqMap.put(leftChar, freqMap.get(leftChar) - 1);
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
