e4229602b02cbf2de7b819402a0dce66549a06d4
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-06-28 17:37:36 +0100

    Add: minimum-number-of-moves-to-seat-everyone - submission-1

---
+++ Data Structures & Algorithms/minimum-number-of-moves-to-seat-everyone/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        int minMoves = 0;
+
+        for (int i = 0; i < seats.length; i++) {
+            if (seats[i] != students[i]) {
+                minMoves += Math.abs(seats[i] - students[i]);
+            }
+        }
+        return minMoves;
+    }
+}
