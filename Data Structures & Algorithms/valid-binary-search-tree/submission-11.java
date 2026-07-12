8898166d90215cab471f23c4bdfd994de5a6d361
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-12 15:50:42 +0100

    Add: valid-binary-search-tree - submission-11

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-11.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        
+        int[] initialInterval = new int[] {-1001, 1001};
+
+        return dfs(root, initialInterval);
+    }
+
+    private boolean dfs(TreeNode root, int[] interval) {
+
+        if (root == null) return true;
+
+        // Out of bounds
+        if (root.val <= interval[0] || root.val >= interval[1]) return false;
+
+        // Within bounds
+
+        return dfs(root.left, new int[]{interval[0], root.val}) && dfs(root.right, new int[]{root.val, interval[1]});
+
+    }
+}
