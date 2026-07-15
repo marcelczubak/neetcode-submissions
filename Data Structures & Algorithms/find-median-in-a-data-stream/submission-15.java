2f7d98e50b1566bd1b9cb9a801e82de52046c8fd
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-15 15:46:40 +0100

    Add: find-median-in-a-data-stream - submission-15

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-15.java
@@ -0,0 +1,33 @@
+class MedianFinder {
+
+    // For left half
+    PriorityQueue<Integer> maxHeap;
+    // For right half
+    PriorityQueue<Integer> minHeap;
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        if (maxHeap.peek() != null && num < maxHeap.peek()) maxHeap.offer(num);
+        else minHeap.offer(num);
+
+        while (Math.abs(minHeap.size() - maxHeap.size()) > 1) {
+            if (minHeap.size() > maxHeap.size()) {
+                maxHeap.offer(minHeap.poll());
+            } else {
+                minHeap.offer(maxHeap.poll());
+            }
+        }
+    }
+    
+    public double findMedian() {
+        
+        if (maxHeap.size() == minHeap.size()) return (maxHeap.peek() + minHeap.peek()) / 2.0;
+
+
+        return (maxHeap.size() > minHeap.size()) ? maxHeap.peek() : minHeap.peek();
+    }
+}
