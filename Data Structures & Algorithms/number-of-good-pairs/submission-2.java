d661e58e3e324f2ffa1520a9b7c721a250234648
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-16 23:29:44 +0100

    Add: number-of-good-pairs - submission-2

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int goodPairs = 0;
+
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0)+1);
+        }
+
+        for (int num : freqMap.keySet()) {
+            int freq = freqMap.get(num);
+
+
+            for (int i = 1; i < freq; i++) {
+                goodPairs += i;
+            }
+
+
+
+        }
+
+        return goodPairs;
+
+    }
+}
