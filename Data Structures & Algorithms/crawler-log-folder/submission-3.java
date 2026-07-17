aabfcd8233cad0126a8114683431ce09fe22fb31
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-18 00:55:22 +0100

    Add: crawler-log-folder - submission-3

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        
+        Stack<String> logStack = new Stack<>();
+
+        for (String log : logs) {
+            if (log.equals("./")) {
+                continue;
+            } else if (log.equals("../")) {
+                if (!logStack.isEmpty()) logStack.pop();
+            } else {
+                logStack.push(log);
+            } 
+        }
+        return logStack.size();
+    }
+}
