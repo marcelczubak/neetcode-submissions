84cd139200050febb16aabffd168eb67a7aa3409
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-18 01:19:30 +0100

    Add: palindrome-linked-list - submission-12

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-12.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        
+        Stack<Integer> stack = new Stack<>();
+
+        int length = 0;
+
+        ListNode curr = head;
+        while (curr != null) {
+            length++;
+            curr = curr.next;
+        }
+
+        boolean odd = (length % 2 != 0);
+
+        curr = head;
+        int index = 0;
+
+        while (curr != null) {
+            if (odd && index == length/2) ;
+
+            else if (index < length/2) stack.push(curr.val);
+
+            else if (index >= length/2 && curr.val != stack.pop()) return false;
+
+           
+
+            index++;
+            curr = curr.next;
+        }
+        return true;
+    }
+}
