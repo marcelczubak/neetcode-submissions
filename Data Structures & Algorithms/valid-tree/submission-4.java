78f519b4430e699b1ac2c6f7cb91ffeae0244bbb
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-18 21:12:02 +0100

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+
+        // Tree : Undirected and ACYCLIC
+
+        // Create adjacency list
+        Map<Integer, Set<Integer>> adjacencyList = new HashMap<>();
+        for (int[] edge : edges) {
+            adjacencyList.putIfAbsent(edge[0], new HashSet<>());
+            adjacencyList.putIfAbsent(edge[1], new HashSet<>());
+
+            // Add both edge directions since undirected
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+
+        }
+
+        // Set used for cycle detection
+        Set<Integer> visited = new HashSet<>();
+
+        if (!dfs(visited, adjacencyList, 0, -1)) return false;
+
+        return (visited.size() == n);
+
+    }   
+
+    private boolean dfs(Set<Integer> visited, Map<Integer, Set<Integer>> adjacencyList, int current, int parent) {
+        
+        // Cycle
+        if (visited.contains(current)) return false;
+
+        visited.add(current);
+
+        Set<Integer> neighbors = adjacencyList.getOrDefault(current, new HashSet<>());
+
+        for (int neighbor : neighbors) {
+            if (parent == neighbor) continue;
+            if (!dfs(visited, adjacencyList, neighbor, current)) return false;
+        }
+
+        return true;
+
+    }
+
+
+
+
+}
