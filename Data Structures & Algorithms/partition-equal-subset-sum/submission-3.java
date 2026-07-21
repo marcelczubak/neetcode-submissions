cebe059e520d8c5ac4180f71b6b1745bd68f4a5e
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-22 00:01:27 +0100

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+
+        Map<String, Boolean> memo = new HashMap<>();
+        
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        // If sum not even, partition not possible
+        if (sum % 2 != 0) return false;
+
+        return partition(memo, nums, 0, 0, sum/2);
+
+    }
+
+    // Try to form a subset which adds up to target
+    private boolean partition(Map<String, Boolean> memo, int[] nums, int i, int curSum, int target) {
+        
+        if (curSum == target) return true;
+        if (i >= nums.length || curSum > target) return false;
+
+        String key = String.valueOf(i) + "." + String.valueOf(curSum);
+        if (memo.containsKey(key)) return memo.get(key);
+
+        boolean takeCurrent = partition(memo, nums, i+1, curSum + nums[i], target);
+
+        boolean skipCurrent = partition(memo, nums, i+1, curSum, target);
+
+        memo.put(key, takeCurrent || skipCurrent);
+
+        return takeCurrent || skipCurrent;
+
+    }
+
+}
