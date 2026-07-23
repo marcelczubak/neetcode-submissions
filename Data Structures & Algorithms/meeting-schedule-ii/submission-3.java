85029bf7dac52ee5c3c5f1cb1d6023b66e845946
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-23 15:16:10 +0100

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.java
@@ -0,0 +1,38 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        
+        // Sort intervals based on starting times
+        intervals.sort((a,b) -> Integer.compare(a.start, b.start));
+
+        
+        // Min-Heap keeps track of first available room
+        PriorityQueue<Integer> rooms = new PriorityQueue<>();
+
+        for (Interval interval : intervals) {
+
+            if (rooms.isEmpty()) {
+                rooms.add(interval.end);
+                continue;
+            }
+
+            if (rooms.peek() <= interval.start) {
+                rooms.poll();
+            }
+                 
+            rooms.add(interval.end);
+        }
+
+        return rooms.size();
+    }
+}
