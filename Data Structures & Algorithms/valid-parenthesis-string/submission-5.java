590f62a27aa7e58824b129f52921d8bc83b669d1
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-26 00:33:43 +0200

    Add: valid-parenthesis-string - submission-5

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-5.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char token = s.charAt(i);
+
+            if (token == '(') leftStack.push(i);
+            if (token == '*') starStack.push(i);
+
+            if (token == ')') {
+
+                if (leftStack.isEmpty() && starStack.isEmpty()) return false;
+
+                if (!leftStack.isEmpty()) leftStack.pop();
+                else if (!starStack.isEmpty()) starStack.pop();
+            }
+        }
+
+        if (starStack.size() < leftStack.size()) return false;
+
+        while (!leftStack.isEmpty() && !starStack.isEmpty()) {
+
+            if (leftStack.pop() > starStack.pop()) return false;
+
+        }
+
+        return true;
+    }
+}
