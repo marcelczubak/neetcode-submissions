9745389e492780ee4319332635a72ba3254abd99
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-07-29 15:16:35 +0200

    Add: sum-of-two-integers - submission-4

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getSum(int a, int b) {
+
+        int result = 0;
+        int carry = 0;
+        
+        for (int i = 0; i < 32; i++) {
+
+            int bitA = (a >> i) & 1;
+            int bitB = (b >> i) & 1;
+
+            int xor = bitA ^ bitB ^ carry;
+
+            carry = (bitA & bitB) | (bitA & carry) | (bitB & carry);
+
+            result |= (xor << i);
+        }
+
+        return result;
+    }
+}
