524aaa66e1cd4a277d80a0ff59f39b3edd47cb53
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-08-03 18:24:04 +0200

    Add: coin-change-ii - submission-19

---
+++ Data Structures & Algorithms/coin-change-ii/submission-19.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+
+        Integer[][] cache = new Integer[coins.length][amount+1];
+        
+        int ways = 0;
+
+        ways += tryCombinations(cache, 0, amount, coins);
+
+        return ways;
+
+    }
+
+    private int tryCombinations(Integer[][] cache, int currentIndex, int amount, int[] coins) {
+        
+        if (amount < 0) return 0;
+        if (cache[currentIndex][amount] != null) return cache[currentIndex][amount];
+
+        if (amount == 0) return 1;
+
+        int ways = 0;
+
+        for (int i = currentIndex; i < coins.length; i++) {
+            ways += tryCombinations(cache, i, amount - coins[i], coins);
+        }
+
+        cache[currentIndex][amount] = ways;
+
+        return ways;
+    }
+
+    private String stringify(int a, int b) {
+        return (String.valueOf(a) + "," + String.valueOf(b));
+    }
+
+}
