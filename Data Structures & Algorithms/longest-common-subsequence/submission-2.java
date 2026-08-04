79aa078bfd54ef4a5431f917721b337903da57ee
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-08-04 23:15:05 +0200

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        // [i][j] -> lcs(i, j)
+        Integer[][] cache = new Integer[text1.length()+1][text2.length()+1];
+
+        return lcs(cache, text1, text2, 0, 0);
+    }
+
+    private int lcs(Integer[][] cache, String text1, String text2, int i, int j) {
+
+        if (cache[i][j] != null) return cache[i][j];
+
+        if (i == text1.length() || j == text2.length()) return 0;
+
+        int result = 0;
+
+        // Characters match
+        if (text1.charAt(i) == text2.charAt(j)) result = 1 + lcs(cache, text1, text2, i+1, j+1);
+        // Characters don't match
+        else result = Math.max(lcs(cache, text1, text2, i+1, j), lcs(cache, text1, text2, i, j+1));
+
+        cache[i][j] = result;
+
+        return result;
+    }
+}
