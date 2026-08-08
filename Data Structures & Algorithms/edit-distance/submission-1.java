1f1f487c3e77f7db303fa2ad3ad4ce6fb7ed414f
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-08-08 09:16:13 +0200

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+
+        Integer[][] cache = new Integer[word1.length()+1][word2.length()+1];
+
+        return getMinDist(cache, word1, word2, 0, 0);
+    }
+
+    private int getMinDist(Integer[][] cache, String word1, String word2, int i, int j) {
+
+        if (i >= word1.length()) return word2.length() - j;
+        if (j >= word2.length()) return word1.length() - i;
+
+        if (cache[i][j] != null) return cache[i][j];
+
+        if (word1.charAt(i) == word2.charAt(j)) return getMinDist(cache, word1, word2, i+1, j+1);
+
+        int insert, delete, replace, result;
+
+        insert = getMinDist(cache, word1, word2, i, j+1);
+        delete = getMinDist(cache, word1, word2, i+1, j);
+        replace = getMinDist(cache, word1, word2, i+1, j+1);
+        
+        if (insert < delete) result = 1+ Math.min(insert, replace);
+        else result = 1+ Math.min(delete, replace);
+
+        cache[i][j] = result;
+        return result;
+    }
+
+}
