1a5722638d88803625458749d781bfab36251b83
Marcel Czubak <105883828+marcelczubak@users.noreply.github.com>
2026-08-20 15:34:43 +0100

    Add: excel-sheet-column-title - submission-16

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-16.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        
+        StringBuilder result = new StringBuilder();
+
+        int current = columnNumber;
+
+        while (current > 0) {
+
+            current--;
+
+            int res = current / 26;
+            int rem = current % 26;
+
+            result.append((char) (rem + 'A'));
+
+            current = res;
+        }
+        
+        return result.reverse().toString();
+    }
+}
